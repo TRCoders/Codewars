bb0e79dbedf04dbc1a7f3dfad6b728ac4d595930
TRCoders <TRCoders@users.noreply.github.com>
2021-09-17 20:10:17 -0400

    Codewarscommit

---
+++ Week 2/day5.js
@@ -0,0 +1,9 @@
+// Grasshopper - Personalized Message
+
+function greet (name, owner) {
+  if (name !== 'Greg' && name === 'Daniel'){
+    return 'Hello boss'
+  } else {
+    return 'Hello guest'
+  }
+}
