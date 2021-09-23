aa10079823c3c790c1bf217c76837ad0421b07b9
TRCoders <TRCoders@users.noreply.github.com>
2021-09-22 21:22:37 -0400

    week3commit

---
+++ Week 3/day3.js
@@ -0,0 +1,11 @@
+// Thinkful - Logic Drills: Traffic light
+
+function updateLight(current) {
+  if (current === "green"){
+      return "yellow"
+  } else if (current === "yellow") {
+      return "red"
+  } else if (current === "red"){
+      return "green"
+  }
+}
