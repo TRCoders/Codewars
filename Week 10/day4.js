a6f25cabec99f1a7f3a35dd51e324a2892c8c55a
Tyler Robinson <tyler.robinson.business@gmail.com>
2021-12-27 17:35:57 -0500

    update

---
+++ Week 10/day4.js
@@ -0,0 +1,10 @@
+// Find the calculation type
+
+function calcType(a, b, res) {
+  switch (res) {
+      case a + b: return 'addition'
+      case a - b: return 'subtraction'
+      case a * b: return 'multiplication'
+      case a / b: return 'division'
+  }
+}
