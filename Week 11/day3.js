494c952a538b4f660e2c0453ad0073f272b59fef
Tyler Robinson <tyler.robinson.business@gmail.com>
2021-12-30 23:59:14 -0500

    update

---
+++ Week 11/day3.js
@@ -0,0 +1,9 @@
+// Return the Missing Element
+
+function getMissingElement(superImportantArray){
+  for (let i = 0; i < 10; i++) {
+    if (!superImportantArray.includes(i)) {
+      return i
+    }
+  }
+}
