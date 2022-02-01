92d513ac1c35a7f797fbba7f0b69d68295653e0e
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-01-31 23:46:41 -0500

    update

---
+++ Week 16/day2.js
@@ -0,0 +1,16 @@
+// Grasshopper - Grade book
+
+function getGrade (s1, s2, s3) {
+  let grade = (s1 + s2 + s3) / 3
+  if (grade >= 90) {
+    return 'A'
+  } else if (grade >= 80) {
+    return 'B'
+  } else if (grade >= 70) {
+    return 'C'
+  } else if (grade >= 60) {
+    return 'D'
+  } else {
+    return 'F'
+  }
+}
